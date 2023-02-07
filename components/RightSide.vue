7a982ea9cf17836d9cbdf6ad3c1624a606e1fa27
manik <mdmanik081498@gmail.com>
2023-02-07 16:22:14 +0600

    facebook-clone added to messenger

--- components/RightSide.vue
+++ components/RightSide.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="right-container">
         <div class="right-container-body">
+            <p class="sponsor-header">Sponsored</p>
+            <div class="sponsor-body">
+                <img src="https://glittering-fenglisu-411425.netlify.app/_nuxt/img/architecture.b5fbfbe.jpg" alt="">
+                <div class="sponsor-body-content">
+                    <p class="sponsor-title">twelve open architecture competition
+                        architecturecompetitions.com </p>
+                    <!-- <p class="name-title">birthday is today.</p> -->
+                </div>
+            </div>
+
+
             <p class="content-header">Birthdays</p>
             <div class="birthday-body">
                 <i data-visualcompletion="css-img" class="x1b0d499 xl1xv1r"
@@ -22,11 +33,111 @@
                 </div>
 
                 <div class="contact-men">
-                    <img class="men-profile" src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/309860881_106948362201197_6916636301056234676_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mY03I4fG-PwAX_Aua-p&tn=q8ZIaHmfuPKs0I_d&_nc_ht=scontent.frjh1-1.fna&oh=00_AfB-WutPJSXZVZoZI7OXRfijmbiZ0yBL-72k0K-K-mKvFQ&oe=63E7DACB"
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/326220603_561090076034550_151460372335431863_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=P34eS2OS9RUAX8hNJtg&tn=q8ZIaHmfuPKs0I_d&_nc_ht=scontent.frjh1-1.fna&oh=00_AfBQ2CeEjQEMawTt6R4A6dbNJI3TbM56DcToi70sd5IAXw&oe=63E77B02"
                         alt="">
-                        <div class="dash-active"></div>
-                    <p class="men-title">Md Zubayer Shekh</p>
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Nasim Uddin</p>
                 </div>
+
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/309860881_106948362201197_6916636301056234676_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=mY03I4fG-PwAX_Aua-p&tn=q8ZIaHmfuPKs0I_d&_nc_ht=scontent.frjh1-1.fna&oh=00_AfB-WutPJSXZVZoZI7OXRfijmbiZ0yBL-72k0K-K-mKvFQ&oe=63E7DACB"
+                        alt="">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Zubayer Sheikh</p>
+                </div>
+                <div class="contact-men">
+                    <img src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/324934781_700578214808071_5708330825407953835_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=UGift6oqcCcAX9Y3Ipq&_nc_ht=scontent.frjh1-1.fna&oh=00_AfAIxpJMXnlTzABTwj9EtLpTPRtEPKnJDnovFtKqXxgNsQ&oe=63E7E313"
+                        alt="" class="men-profile">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Safayet Shohag</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/329217787_855998199012166_6910339888022370133_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=kqBGbl0kZ7wAX-qYnBM&_nc_ht=scontent.frjh1-1.fna&oh=00_AfBEld9A5IkqqDo9EQy8uP59pBJxl7oZMcRWhXMpboypFg&oe=63E76CA1">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Sm Monir Sheikh</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/327095953_5780239092067605_3029027462063571213_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=6CQu7CLZM0EAX93P9Uy&_nc_ht=scontent.frjh1-1.fna&oh=00_AfCoiUY_daqUxmUMrc6-bFET-Dbs9gvoKsjSWBejMjfOig&oe=63E759E9">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Shamim Hosain</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/321514942_1420424215154877_8833582797156764263_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lrhc_UIuL70AX92Beiz&_nc_ht=scontent.frjh1-1.fna&oh=00_AfB8r9pkLJ2aFUc8uXgkTX4GIm0wQppelCEIN3Gxtyihig&oe=63E7BF51">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Siyam Sheikh</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/327175073_969532710690898_7410797916946758193_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=70CutFlKGhwAX868b-Y&_nc_ht=scontent.frjh1-1.fna&oh=00_AfD6L8kpDHAUTSBddGO9D0jXiOyk6tlnm7NTIbkAWpgq_A&oe=63E68C20">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Nasir Uddin</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/309389927_1042357079713409_806756507668591166_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV1fVDr7QLYAX9B71gh&_nc_ht=scontent.frjh1-1.fna&oh=00_AfC90BzRtMgM64TJYMD9wI61snbHoMIU3xhkhYfpZb0jrg&oe=63E7E3FD">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Masud Rana</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/327283666_483383357326132_5663460644255388305_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qoFt7mL8u4cAX9KeRp2&_nc_ht=scontent.frjh1-1.fna&oh=00_AfCDsHKHuzXkeWYez38hb_DWNLh4_lh8NqTQK5TD2zmkYg&oe=63E7456A">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Abir Hasan Sani</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/319857467_920774835576693_7032319010293849763_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=9Q79-VMs2-QAX91Hp_w&_nc_ht=scontent.frjh1-1.fna&oh=00_AfCwRKWdGx2pBCVBpqCZDsmRnaIKM_1N9SxNSAYhMAq8yw&oe=63E799ED">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Jibon Ahmed Shiblu</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/325158601_682017866902864_2346445026249708687_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=GkHIcWmgWoYAX8rEsoq&_nc_ht=scontent.frjh1-1.fna&oh=00_AfCfw9t5cJLwzluIAj6txsdbcQGD0PLiiV3oDGSyCnbcKA&oe=63E75A94">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Mahmudul Islam </p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/313337620_647007480200687_7837686830118023406_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=O8XT0s_e1xkAX-cptq3&_nc_ht=scontent.frjh1-1.fna&oh=00_AfACgIPcGVJGONz6Kk3XyH_X7vo9-MK8L56qN6eZAtrFQg&oe=63E647FC">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Rohan Sheikh</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/271767253_1311471179366330_7360195406424222870_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=HyWfsvNuEwgAX8L9K6r&tn=q8ZIaHmfuPKs0I_d&_nc_ht=scontent.frjh1-1.fna&oh=00_AfBw_D5rNiljRxLW46r3bHrQkuq993vKAom8xR9PByAWjQ&oe=63E76317">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Rabbi Hosain</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/279316467_1114604346051332_8794568449286206057_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lqj-UeEpHiMAX9VGklD&_nc_ht=scontent.frjh1-1.fna&oh=00_AfByr7EO96LYMRCRK3aQ2F74r8wm22iCb-zCfRjbXeWPiQ&oe=63E73751">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Akram Sheikh</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t1.6435-9/134586512_155235432854397_5423114427450656243_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=q0LTnWI8OIoAX8J3rfA&tn=q8ZIaHmfuPKs0I_d&_nc_ht=scontent.frjh1-1.fna&oh=00_AfCZPshM0IK3Ezkq_PCdSAAHbRHLb7crzFmZhADEIJSYng&oe=64098C67">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Mst Nur Saniya</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/315751917_1555420124891499_3549429192355807327_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ws81eUCJ67AAX_ElNp5&_nc_ht=scontent.frjh1-1.fna&oh=00_AfDwUo5bbUCPAvP2Bj7-l91QEnVN39sImsO1Rl8HLkYajw&oe=63E7065A">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Mehidi Hasan Shuvo</p>
+                </div>
+                <div class="contact-men">
+                    <img class="men-profile"
+                        src="https://scontent.frjh1-1.fna.fbcdn.net/v/t39.30808-6/326997082_891855468898507_3092119291818199177_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=fRRV4Gm0idEAX8EpSU-&_nc_ht=scontent.frjh1-1.fna&oh=00_AfD54_G665PrE3mhrckatgJEaCvqGSTv0opd8QProGAC4g&oe=63E6D741">
+                    <div class="dash-active"></div>
+                    <p class="men-title">Md Foysal Sheikh</p>
+                </div>
+
             </div>
         </div>
 
@@ -55,39 +166,60 @@ export default {
         // display: flex;
         padding-right: 10px;
 
-        .content-header {
+        .sponsor-header {
             font-size: 18px;
             color: gray;
             font-weight: bold;
         }
 
-        .birthday-body {
-            display: flex !important;
-            align-items: center;
-            justify-content: space-between !important;
+        .sponsor-body {
 
-            &:hover {
-                background: rgb(212, 216, 214);
-                cursor: pointer;
-                border-radius: 10px;
-            }
+            .sponsor-body-content {
 
-            .birthday-body-content {
-                display: flex;
-                padding: 6px 0 0 15px;
-                line-height: 0.6;
-
-                .name {
-                    font-size: 16px;
-                    font-weight: bold;
-                }
-
-                .name-title {
-                    font-size: 15px;
-                    padding-left: 15px;
+                .sponsor-title {
+                    font-size: 17px;
+                    line-height: 1;
+                    color: rgb(146, 139, 139);
+                
                 }
             }
         }
+
+        .content-header { 
+            font-size: 18px;
+            color: gray;
+            font-weight: bold;
+        }
+            .birthday-body {
+                display: flex !important;
+                align-items: center;
+                justify-content: space-between !important;
+
+                &:hover {
+                    background: rgb(212, 216, 214);
+                    cursor: pointer;
+                    border-radius: 10px;
+                }
+
+                .birthday-body-content {
+                    display: flex;
+                    padding: 6px 0 0 15px;
+                    line-height: 0.6;
+
+                    .name {
+                        font-size: 16px;
+                        font-weight: bold;
+                    }
+
+                    .name-title {
+                        font-size: 15px;
+                        padding-left: 15px;
+                    }
+                }
+            }
+
+       
+
 
         .contact-body {
             display: inline;
@@ -144,6 +276,7 @@ export default {
             }
 
             .contact-men {
+                margin-top: 1px;
                 display: flex;
                 align-items: center;
                 padding: 5px;
@@ -155,31 +288,29 @@ export default {
                 }
 
                 .men-profile {
-                    height: 40px;
-                    width: 40px;
+                    height: 45px;
+                    width: 45px;
                     border-radius: 50%;
                     position: relative;
                 }
 
                 .dash-active {
-                    height:10px;
-                    width:10px;
+                    height: 10px;
+                    width: 10px;
                     border-radius: 5px;
                     background: green;
                     top: 15px;
-                    right:9px;
+                    right: 9px;
                     position: relative;
                 }
+
                 .men-title {
                     font-size: 16px;
                     color: rgb(66, 65, 65);
-                    
-                    padding-left: 20px;
+                    padding-left: 15px;
                 }
             }
         }
-
-
 
     }
 }
