327a02a93a4306dd2bfdfe71a079a480bfce9e1e
maniksheikh <dev.maniksheikh@gmail.com>
2025-08-07 19:01:43 +0600

    remove unnececry code

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,6 @@
         <div v-if="error" class="error">
           {{ error }}
         </div>
-
         <form action="" @submit.prevent="loginUser">
           <input v-model="user.email" type="email" placeholder="Email address or phone number" required />
           <input v-model="user.password" type="password" name="" placeholder="Password" required />
@@ -142,7 +141,6 @@ export default {
       cursor: pointer;
 
       .content-box {
-
         p {
           padding-top: 1rem;
           font-size: 1.1rem;
@@ -158,7 +156,6 @@ export default {
         display: flex;
         flex-direction: column;
         border-radius: 50px;
-
         border: 1px solid rgba(130, 117, 117, 0.2);
         border-radius: 5px;
 
@@ -179,7 +176,6 @@ export default {
 
         .content-box {
           background: white;
-
           text-align: center;
           width: 100%;
           box-sizing: border-box;
@@ -202,7 +198,6 @@ export default {
           border-top-right-radius: 5px;
           border-top-left-radius: 5px;
           position: relative;
-
           .add-btn {
             position: absolute;
             top: 50%;
@@ -226,16 +221,13 @@ export default {
     background: white;
     box-sizing: border-box;
     box-shadow: 0 0 10px rgba(130, 117, 117, 0.2);
-
     ul li {
       margin-bottom: 1.5rem;
       margin-top: 2rem;
     }
-
     a {
       text-decoration: none;
     }
-
     p {
       color: #1877f2;
       text-align: center;
@@ -244,7 +236,6 @@ export default {
       letter-spacing: 1px;
       font-family: Helvetica, Arial, sans-serif;
       border-bottom: 1px solid rgba(130, 117, 117, 0.3);
-
       &:hover {
         text-decoration: underline;
       }
@@ -257,11 +248,9 @@ export default {
       padding: 0.9rem 0.5rem;
       margin-bottom: 15px;
       font-size: 1rem;
-      // border: 1px solid transparent !important;
       border: 1px solid rgba(130, 117, 117, 0.2);
       border-radius: 5px;
       outline: none;
-
       &:focus {
         border: 1px solid blue;
       }
@@ -277,7 +266,6 @@ export default {
       border-radius: 5px;
       transition: 0.3s ease-in;
       font-family: Helvetica, Arial, sans-serif;
-
       &:hover {
         background: #005fdb;
       }
@@ -297,7 +285,6 @@ export default {
       font-weight: 700;
       border-radius: 5px;
       transition: 0.3s ease-in-out;
-
       &:hover {
         background: #40bc27;
       }
@@ -314,18 +301,14 @@ export default {
       align-items: center;
       gap: 1rem;
     }
-
     .accounts {
       margin-right: 0rem !important;
-
       .center-text {
         text-align: center;
         line-height: 0;
-
         h1 {
           font-size: 28px;
         }
-
         p {
           font-size: 14px;
         }
